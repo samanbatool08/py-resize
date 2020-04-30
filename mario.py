da794bcc4ce9c9c99463b6cc4e999d2fbee6c9a9
Your Name <samanbatool08@gmail.com>
2020-04-30 12:38:39 -0400

    loops within loops

---
+++ mario.py
@@ -0,0 +1,8 @@
+print("????")
+
+# Same as above in for loop
+for i in range(4):
+    print("?")
+# Prints new line for free
+print()
+
---
+++ mario1.py
@@ -0,0 +1,12 @@
+# print 1 by 3
+for i in range(3):
+    print("#")
+
+# print 3 by 3
+
+# 3 times vertically
+for i in range(3):
+    # 3 times horizontally 
+    for j in range(3):
+        print("#")
+    print()
